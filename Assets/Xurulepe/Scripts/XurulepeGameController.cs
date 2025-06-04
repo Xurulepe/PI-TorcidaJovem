45ae8d52167b2e6be4110931bd5ba4fb3580b545
Xurulepe <xurulepe@gmail.com>
2025-06-04 20:45:06 -0300

    project settings

---
+++ Assets/Xurulepe/Scripts/XurulepeGameController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class XurulepeGameController : MonoBehaviour
+{
+    public Transform _player;
+    [SerializeField] Transform[] _iniPosV;
+    [SerializeField] int _posNumb;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void SpawnEnemy()
+    {
+
+    }
+}
