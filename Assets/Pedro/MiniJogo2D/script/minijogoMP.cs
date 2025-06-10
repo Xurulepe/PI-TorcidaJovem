67e562dc84b41a45f5948cac7fe79ba8d3432b70
Xurulepe <xurulepe@gmail.com>
2025-06-09 21:22:36 -0300

    Controle de animação
    + Controle de animação por script.

--- Assets/Pedro/MiniJogo2D/script/minijogoMP.cs
+++ Assets/Pedro/MiniJogo2D/script/minijogoMP.cs
@@ -6,6 +6,9 @@ public class minijogoMP : MonoBehaviour
     [Header("física")]
     [SerializeField] Rigidbody2D _rb;
 
+    [Header("animations")]
+    [SerializeField] Animator _anim;
+
     [Header("movimento")]
     [SerializeField] Vector2 _move;
     [SerializeField] float _speed;
@@ -13,7 +16,8 @@ public class minijogoMP : MonoBehaviour
     [Header("force pulo")]
     [SerializeField] float _forceJump;
 
-    [SerializeField] bool _checkGround;
+    [SerializeField] bool _isGrounded;
+    [SerializeField] bool _isWalking;
 
     void Start()
     {
@@ -24,19 +28,26 @@ public class minijogoMP : MonoBehaviour
     void Update()
     {
         _rb.linearVelocity = new Vector2(_move.x * _speed, _rb.linearVelocity.y);
+
+        Flip();
+        _anim.SetBool("isWalking", _isWalking);
+        _anim.SetBool("isGrounded", _isGrounded);
     }
 
 
     public void SetMove(InputAction.CallbackContext value)
     {
         _move = value.ReadValue<Vector2>();
+        _isWalking = value.performed;
     }
 
     #region função de pulo
     void Jump()
     {
+        _isGrounded = false;
         _rb.linearVelocityY = 0;
         _rb.AddForceY(_forceJump);
+        _anim.SetTrigger("jump");
     }
     #endregion
 
@@ -48,9 +59,25 @@ public class minijogoMP : MonoBehaviour
         {
             Jump();
         }
+        else if (collision.CompareTag("ground"))
+        {
+            _isGrounded = true;
+        }
     }
     #endregion
 
-
+    #region flip
+    private void Flip()
+    {
+        if (_move.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (_move.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+    #endregion
 
 }
