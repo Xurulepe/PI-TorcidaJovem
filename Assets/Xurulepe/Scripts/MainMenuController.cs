40c42ed7152c7c064cc41f313137fe97829fcd6a
Xurulepe <xurulepe@gmail.com>
2025-06-11 21:10:11 -0300

    Animação Menu com fade
    + Adicionado fade ao iniciar a cena e ao alterar entre cenas.

--- Assets/Xurulepe/Scripts/MainMenuController.cs
+++ Assets/Xurulepe/Scripts/MainMenuController.cs
@@ -1,35 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
 
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] List<Transform> menuButtons = new List<Transform>();
+    [SerializeField] Image fadePanel;
 
     private void Start()
     {
         foreach(var button in menuButtons)
         {
-            button.localScale = Vector3.zero;
+            //button.localScale = Vector3.zero;
+            button.localScale = new Vector3(button.localScale.x, 0f, button.localScale.z);
         }
 
+        FadeToLight();
         StartCoroutine(MenuON());
     }
 
     private IEnumerator MenuON()
     {
+        yield return new WaitForSeconds(1.25f);
+
         foreach (var button in menuButtons)
         {
-            button.DOScale(1.5f, .25f);
+            //button.DOScale(1.5f, .25f);
+            //yield return new WaitForSeconds(.25f);
+            //button.DOScale(1f, .25f);
+            button.DOScaleY(1f, .25f);
             yield return new WaitForSeconds(.25f);
-            button.DOScale(1f, .25f);
         }
+    }
+
+    private void FadeToLight()
+    {
+        //fadePanel.color = new Color(0f, 0f, 0f, 255f);
+        fadePanel.DOFade(0f, 2.5f).OnComplete(HideFadePanel);
+    }
+
+    private void FadeToDark()
+    {
+        //fadePanel.color = new Color(0f, 0f, 0f, 0f);
+
+        fadePanel.DOFade(1f, 1f);
+    }
+
+    void ShowFadePanel()
+    {
+        fadePanel.transform.localScale = Vector3.one;
+    }
+
+    void HideFadePanel()
+    {
+        fadePanel.transform.localScale = Vector3.zero;
     }
 
     public void LoadScene(string sceneName)
     {
+        //SceneManager.LoadScene(sceneName);
+        StartCoroutine(OpenScene(sceneName));
+    }
+
+    IEnumerator OpenScene(string sceneName)
+    {
+        ShowFadePanel();
+        fadePanel.color = new Color(0f, 0f, 0f, 0f);
+        FadeToDark();
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(sceneName);
     }
 
