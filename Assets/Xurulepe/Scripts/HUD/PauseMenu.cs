c9799be8d26227b7fab8d671c328106831eae03d
Xurulepe <xurulepe@gmail.com>
2025-10-20 16:24:02 -0300

    Animações no menu de pausa
    + Adicionado animações ao navegar pelo menu de pausa.
    + Prefab do menu de pausa em: Assets\Xurulepe\Prefabs\UI
    ! O prefab requer o AudioManager presente na cena para reproduzir os sons, e EventSystem para funcionar.

--- Assets/Xurulepe/Scripts/HUD/PauseMenu.cs
+++ Assets/Xurulepe/Scripts/HUD/PauseMenu.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _pauseMenuPanel;
 
+    // propriedade estática para verificar se o jogo está pausado
+    // substituir por não-static em um GameManager depois
+    public static bool IsPaused { get; private set; } = false;
 
     // teste de música
     public AudioClip music;
@@ -17,23 +21,42 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
+    #region PAUSE/RESUME/EXIT
     public void Pause()
     {
         Debug.Log("Pausando o jogo");
-        _pauseMenuPanel.SetActive(true);
-        Time.timeScale = 0f;  // Pausa o jogo
+        IsPaused = true;
+        AnimatePauseMenuIn(_pauseMenuPanel);
     }
 
     public void Resume()
     {
         Debug.Log("Retomando o jogo");
-        _pauseMenuPanel.SetActive(false);
-        Time.timeScale = 1f;  // Retoma o jogo
+        IsPaused = false;
+        AnimatePauseMenuOut(_pauseMenuPanel);
     }
 
     public void BackToMenu()
     {
-        Time.timeScale = 1f;  
+        IsPaused = false;
         SceneManager.LoadScene(0);
     }
+    #endregion
+
+    #region ANIMATIONS
+    public void AnimatePauseMenuIn(GameObject gameObject)
+    {
+        gameObject.transform.localScale = Vector3.zero;
+        gameObject.SetActive(true);
+        gameObject.transform.DOScale(Vector3.one, 0.5f).SetEase(Ease.OutBack);
+    }
+
+    public void AnimatePauseMenuOut(GameObject gameObject)
+    {
+        gameObject.transform.DOScale(Vector3.zero, 0.25f).OnComplete(() =>
+        {
+            gameObject.SetActive(false);
+        });
+    }
+    #endregion
 }
