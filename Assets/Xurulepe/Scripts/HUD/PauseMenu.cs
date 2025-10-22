69e2d0801820b2c551849317e007d487472f61c5
Xurulepe <xurulepe@gmail.com>
2025-10-22 15:34:44 -0300

    Pause menu input system
    + Configurado o menu de pausa para funcionar com input system.
    = Alteração e animação de menus agora é feita por script ao invés de botões no jogo.

--- Assets/Xurulepe/Scripts/HUD/PauseMenu.cs
+++ Assets/Xurulepe/Scripts/HUD/PauseMenu.cs
@@ -1,39 +1,107 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject _pauseButtonUI;
+
+    [Header("Componentes do menu de pausa")]
     [SerializeField] private GameObject _pauseMenuPanel;
+    [SerializeField] private GameObject _settingsMenu;
+    [SerializeField] private GameObject _soundsMenu;
+
+    [Header("Listas de menus")]
+    [SerializeField] private List<GameObject> _menus = new List<GameObject>();
+
+    [Header("Controle de botões")]
+    [SerializeField] private Button _firstButtonPauseMenu;
 
     // propriedade estática para verificar se o jogo está pausado
     // substituir por não-static em um GameManager depois
     public static bool IsPaused { get; private set; } = false;
 
+    [Space(20f)]
     // teste de música
     public AudioClip music;
 
     private void Start()
     {
+        _menus = new List<GameObject>()
+        {
+            _pauseMenuPanel,
+            _settingsMenu,
+            _soundsMenu
+        };
+
         if (music != null)
         {
             AudioManager.Instance.PlayMusic(music);
         }
     }
 
+    public void ChangeMenu(int index)
+    {
+        if (index < 0 || index >= _menus.Count)
+        {
+            Debug.LogWarning($"Menu inválido! Index {index} não existe!");
+            return;
+        }
+
+        foreach (var menu in _menus)
+        {
+            menu.SetActive(false);
+        }
+
+        AnimatePauseMenuIn(_menus[index]);
+    }
+
+    #region INPUT
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    #endregion
+
     #region PAUSE/RESUME/EXIT
     public void Pause()
     {
+        _pauseButtonUI.SetActive(false);
+        _firstButtonPauseMenu.Select();
+
         Debug.Log("Pausando o jogo");
         IsPaused = true;
+
         AnimatePauseMenuIn(_pauseMenuPanel);
     }
 
     public void Resume()
     {
+        _pauseButtonUI.SetActive(true);
+
         Debug.Log("Retomando o jogo");
         IsPaused = false;
-        AnimatePauseMenuOut(_pauseMenuPanel);
+
+        foreach (var menu in _menus)
+        {
+            if (menu.activeSelf)
+            {
+                AnimatePauseMenuOut(menu);
+            }
+        }
     }
 
     public void BackToMenu()
@@ -44,14 +112,14 @@ public class PauseMenu : MonoBehaviour
     #endregion
 
     #region ANIMATIONS
-    public void AnimatePauseMenuIn(GameObject gameObject)
+    private void AnimatePauseMenuIn(GameObject gameObject)
     {
         gameObject.transform.localScale = Vector3.zero;
         gameObject.SetActive(true);
         gameObject.transform.DOScale(Vector3.one, 0.5f).SetEase(Ease.OutBack);
     }
 
-    public void AnimatePauseMenuOut(GameObject gameObject)
+    private void AnimatePauseMenuOut(GameObject gameObject)
     {
         gameObject.transform.DOScale(Vector3.zero, 0.25f).OnComplete(() =>
         {
