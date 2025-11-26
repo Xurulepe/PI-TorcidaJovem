45cac0dccffbea7c3e089d09e6ab6c5df93f5d13
DannyB0y7 <danielsribeiro401@gmail.com>
2025-11-26 19:37:51 -0300

    Respawn Player

--- Assets/Daniboy/Scripts/PlayerHealthScript.cs
+++ Assets/Daniboy/Scripts/PlayerHealthScript.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerHealthScript : MonoBehaviour
 {
     public int _maxHealth;
+    public int Respawn;
     public int _currentHealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,8 +14,39 @@ public class PlayerHealthScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Death();
+        Test();
         
     }
+
+    public void Death() 
+    { 
+    
+    if (_currentHealth == 0) 
+    {
+
+       SceneManager.LoadScene(Respawn);
+        
+        
+        
+    }
+    
+    }
+    public void Test() 
+    { 
+    
+    if (Input.GetKeyUp(KeyCode.Escape)) 
+        {
+
+            _currentHealth = 0;
+        
+        
+        
+        }
+    
+    
+    }
+
 
     public void DamagePlayer(int Hurt, Vector3 direction) 
     {
