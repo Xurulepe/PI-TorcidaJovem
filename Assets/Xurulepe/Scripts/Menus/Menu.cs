b1c3adf59d84a9bed27b7e3673c9798fb0e9e1b4
Xurulepe <xurulepe@gmail.com>
2026-03-10 19:32:31 -0300

    Auto select button
    + Botões do tutorial e HUDs do final do jogo podem ser auto selecionados.

--- Assets/Xurulepe/Scripts/Menus/Menu.cs
+++ Assets/Xurulepe/Scripts/Menus/Menu.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     public List<Transform> animatedElements;
+    public Button autoSelectButton;
+
+    private void OnEnable()
+    {
+        if (autoSelectButton != null)
+        {
+            autoSelectButton.Select();
+        }
+    }
 }
